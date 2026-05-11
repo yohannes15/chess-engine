9e85f89a668be22140cc7a72d1612dd4d8343baa
Yohannes <y.berhane56@gmail.com>
2026-05-11 05:35:16 +0000

    Add generate sliding and leaping

--- src/main/scala/chessengine/logic/MoveGenerator.scala
+++ src/main/scala/chessengine/logic/MoveGenerator.scala
@@ -29,14 +29,48 @@ object MoveGenerator:
       from: Square,
       piece: Piece,
       dir: (Int, Int)
-  ): List[Move] = ???
+  ): List[Move] =
+    val (dr, df) = dir
+    def walk(currRank: Int, currFile: Int): List[Move] =
+      val nextRank = currRank + dr
+      val nextFile = currFile + df
+      Square.fromRankAndFile(nextRank, nextFile) match
+        case None         => Nil // Edge of board
+        case Some(nextSq) =>
+          board.pieces(nextSq.index) match
+            case None =>
+              // Empty! Add move and Keep Walking
+              NormalMove(from, nextSq, piece, None) :: walk(nextRank, nextFile)
+            case Some(target) if target.color != piece.color =>
+              // Opponent! Add capture and STOP
+              List(NormalMove(from, nextSq, piece, Some(target)))
+            case _ =>
+              // Friend! STOP
+              Nil
+    walk(from.rank, from.file)
 
   def generateLeaping(
       board: Board,
       from: Square,
       piece: Piece,
       offset: (Int, Int)
-  ): List[Move] = ???
+  ): List[Move] =
+    val nextRank = from.rank + offset(0)
+    val nextFile = from.file + offset(1)
+    Square.fromRankAndFile(nextRank, nextFile) match
+      case Some(nextSq) =>
+        board.pieces(nextSq.index) match
+          case Some(target) if target.color != piece.color =>
+            // Opponent! CAPTURE
+            List(NormalMove(from, nextSq, piece, Some(target)))
+          case None =>
+            // Empty! Move to this spot
+            List(NormalMove(from, nextSq, piece, None))
+          case _ =>
+            // Friend! STOP
+            Nil
+
+      case None => Nil // Edge of board
 
   def generatePawnMoves(
       board: Board,
