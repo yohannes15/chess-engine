2c44414f5b0f03252439d794ab8b8a7d93273902
Yohannes B <48221308+yohannes15@users.noreply.github.com>
2026-05-19 02:37:15 -0400

    Refactor SearchResult enum to combine cases

--- src/main/scala/chessengine/engine/SearchResult.scala
+++ src/main/scala/chessengine/engine/SearchResult.scala
@@ -3,6 +3,5 @@ package chessengine.engine
 import chessengine.domain.{Color, Move}
 
 enum SearchResult:
-  case CheckMate(winner: Color)
-  case StaleMate
+  case CheckMate, StaleMate
   case BestMove(move: Move, score: Int)
