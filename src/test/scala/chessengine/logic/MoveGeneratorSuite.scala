504a2447909215ced7f7e6924b30dd1708aefa9e
Yohannes <y.berhane56@gmail.com>
2026-05-25 02:39:18 +0000

    finish perft tests

--- src/test/scala/chessengine/logic/MoveGeneratorSuite.scala
+++ src/test/scala/chessengine/logic/MoveGeneratorSuite.scala
@@ -5,72 +5,72 @@ import chessengine.TestHelpers.*
 
 class MoveGeneratorSuite extends munit.FunSuite:
 
-  test("initial position perft depth 1") {
+  test("initial perft depth 1") {
     assertEquals(perft(GameState.initial, 1), 20)
   }
 
-  test("initial position perft depth 2") {
+  test("initial perft depth 2") {
     assertEquals(perft(GameState.initial, 2), 400)
   }
 
-  test("initial position perft depth 3") {
+  test("initial perft depth 3") {
     assertEquals(perft(GameState.initial, 3), 8902)
   }
 
-  test("initial position perft depth 4") {
+  test("initial perft depth 4") {
     assertEquals(perft(GameState.initial, 4), 197281)
   }
 
-  test("KiwiPete test depth 1") {
+  test("KiwiPete perft depth 1") {
     val gs = Fen.parse(kiwiPeteFen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 1), 48)
   }
 
-  test("KiwiPete test depth 2") {
+  test("KiwiPete perft depth 2") {
     val gs = Fen.parse(kiwiPeteFen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 2), 2039)
   }
 
-  test("KiwiPete test depth 3") {
+  test("KiwiPete perft depth 3") {
     val gs = Fen.parse(kiwiPeteFen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 3), 97862)
   }
 
-  test("Position 3 - depth 1") {
+  test("Position 3 perft depth 1") {
     val gs =
-      Fen.parse(endGameWithPassedPawns).getOrElse(fail("failed to parse fen"))
+      Fen.parse(position3Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 1), 14)
   }
 
-  test("Position 3 - depth 2") {
+  test("Position 3 perft depth 2") {
     val gs =
-      Fen.parse(endGameWithPassedPawns).getOrElse(fail("failed to parse fen"))
+      Fen.parse(position3Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 2), 191)
   }
 
-  test("Position 3 - depth 3") {
+  test("Position 3 perft depth 3") {
     val gs =
-      Fen.parse(endGameWithPassedPawns).getOrElse(fail("failed to parse fen"))
+      Fen.parse(position3Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 3), 2812)
   }
 
-  test("Position 3 - depth 4") {
+  test("Position 3 perft depth 4") {
     val gs =
-      Fen.parse(endGameWithPassedPawns).getOrElse(fail("failed to parse fen"))
+      Fen.parse(position3Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 4), 43238)
   }
 
-  test("Position 4 - depth 1") {
-    val gs = Fen.parse(promotionChaos).getOrElse(fail("failed to parse fen"))
+  test("Position 4 perft depth 1") {
+    val gs = Fen.parse(position4Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 1), 6)
   }
 
-  test("Position 4 - depth 2") {
-    val gs = Fen.parse(promotionChaos).getOrElse(fail("failed to parse fen"))
+  test("Position 4 perft depth 2") {
+    val gs = Fen.parse(position4Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 2), 264)
   }
 
-  test("Position 4 - depth 3") {
-    val gs = Fen.parse(promotionChaos).getOrElse(fail("failed to parse fen"))
+  test("Position 4 perft depth 3") {
+    val gs = Fen.parse(position4Fen).getOrElse(fail("failed to parse fen"))
     assertEquals(perft(gs, 3), 9467)
   }
