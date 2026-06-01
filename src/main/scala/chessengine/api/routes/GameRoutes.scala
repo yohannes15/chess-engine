96d9182673948cff72a0cc22e7bda205d6ea5724
Yohannes <y.berhane56@gmail.com>
2026-06-01 13:48:52 +0000

    Add MoveRequest and add /move route

--- src/main/scala/chessengine/api/routes/GameRoutes.scala
+++ src/main/scala/chessengine/api/routes/GameRoutes.scala
@@ -8,10 +8,26 @@ import org.http4s.HttpRoutes
 import org.http4s.dsl.io.*
 import cats.effect.IO
 import org.http4s.circe.CirceEntityEncoder.circeEntityEncoder
+import org.http4s.circe.CirceEntityDecoder.circeEntityDecoder
 import chessengine.api.dto.GameStateResponse
-import chessengine.logic.MoveGenerator
+import chessengine.logic.MoveGenerator.*
+import chessengine.api.dto.MoveRequest
+import java.util.UUID
+import org.http4s.Response
+import chessengine.domain.Move
 
 private[api] class GameRoutes(reg: GameRegistry):
+
+  private def handleMove(id: UUID, mv: Move): IO[Response[IO]] =
+    reg.applyMove(id, mv).flatMap:
+      case None       => NotFound("invalid game uuid")
+      case Some(next) =>
+        Ok(GameStateResponse(
+          fen = Fen.write(next),
+          turn = next.color.toString,
+          legalMoves = allLegalMoves(next).map(_.toUCI)
+        ))
+
   def routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case POST -> Root / "games" =>
       for
@@ -27,8 +43,20 @@ private[api] class GameRoutes(reg: GameRegistry):
             Ok(GameStateResponse(
               fen = Fen.write(gs),
               turn = gs.color.toString,
-              legalMoves = MoveGenerator.allLegalMoves(gs).map(_.toUCI)
+              legalMoves = allLegalMoves(gs).map(_.toUCI)
             ))
           case None => NotFound("invalid game uuid")
+      yield response
+
+    case req @ POST -> Root / "games" / UUIDVar(id) / "move" =>
+      for
+        mvReq <- req.as[MoveRequest]
+        gameState <- reg.lookup(id)
+        response <- gameState match
+          case None     => NotFound("invalid game uuid")
+          case Some(gs) =>
+            allLegalMoves(gs).find(_.toUCI == mvReq.move.toLowerCase) match
+              case None     => BadRequest("illegal move")
+              case Some(mv) => handleMove(id, mv)
       yield response
   }
