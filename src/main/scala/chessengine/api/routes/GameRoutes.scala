cfaca9679837a5e2c930af201f536122985c6996
Yohannes <y.berhane56@gmail.com>
2026-06-01 04:34:21 +0000

    Combine chess and game routes in Main and clean up unneeded resource

--- src/main/scala/chessengine/api/routes/GameRoutes.scala
+++ src/main/scala/chessengine/api/routes/GameRoutes.scala
@@ -1,9 +1,14 @@
 package chessengine.api.routes
 
 import chessengine.game.GameRegistry
+
 import org.http4s.HttpRoutes
+import org.http4s.dsl.io.*
 import cats.effect.IO
 
 private[api] class GameRoutes(registry: GameRegistry):
-  def routes: HttpRoutes[IO] =
-    ???
+  def routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "games" =>
+      println(registry.toString)
+      ???
+  }
