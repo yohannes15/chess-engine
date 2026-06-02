050156dfbf90596aa64c3c2be78ed063ec46c20b
Yohannes <y.berhane56@gmail.com>
2026-06-02 07:24:07 +0000

    format

--- src/main/scala/chessengine/api/dto/ErrorResponse.scala
+++ src/main/scala/chessengine/api/dto/ErrorResponse.scala
@@ -2,4 +2,7 @@ package chessengine.api.dto
 
 import io.circe.Encoder
 
-final case class ErrorResponse(message: String, errors: List[String] = List.empty) derives Encoder
+final case class ErrorResponse(
+    message: String,
+    errors: List[String] = List.empty
+) derives Encoder
